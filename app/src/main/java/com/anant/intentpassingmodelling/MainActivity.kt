0f9f0b4fe779979865e9ac1fbb36c47d85eccbf5
Anant Kanchan <anant4265@gmail.com>
2020-12-09 18:29:11 +0530

    backend consoled

--- app/src/main/java/com/anant/intentpassingmodelling/MainActivity.kt
+++ app/src/main/java/com/anant/intentpassingmodelling/MainActivity.kt
@@ -2,10 +2,23 @@ package com.anant.intentpassingmodelling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var etUsername:EditText
+    lateinit var etPassword:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etUsername=findViewById(R.id.etUsername)
+        etPassword=findViewById(R.id.etPassword)
+        
+
+
+
     }
 }
