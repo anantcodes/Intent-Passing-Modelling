0fad35d98d4c222628977fbb486b9059c8e2e81c
Anant Kanchan <anant4265@gmail.com>
2020-12-09 18:40:13 +0530

    intent extra

--- app/src/main/java/com/anant/intentpassingmodelling/MainActivity.kt
+++ app/src/main/java/com/anant/intentpassingmodelling/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.anant.intentpassingmodelling
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,19 @@ class MainActivity : AppCompatActivity() {
 
         etUsername=findViewById(R.id.etUsername)
         etPassword=findViewById(R.id.etPassword)
-        
+
+        val intent = Intent(this, ShowActivity::class.java)
+
+//attach the key value pair using putExtra to this intent
+
+//attach the key value pair using putExtra to this intent
+        val user_name = "Jhon Doe"
+        intent.putExtra("USER_NAME", user_name)
+
+//starting the activity
+
+//starting the activity
+        startActivity(intent)
 
 
 
