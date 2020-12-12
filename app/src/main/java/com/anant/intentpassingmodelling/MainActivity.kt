7a39d8e076b60b35acbc712fcb353aed6276e7ff
Anant Kanchan <anant4265@gmail.com>
2020-12-12 19:32:10 +0530

    btn backend connected

--- app/src/main/java/com/anant/intentpassingmodelling/MainActivity.kt
+++ app/src/main/java/com/anant/intentpassingmodelling/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,13 +18,22 @@ class MainActivity : AppCompatActivity() {
 
         etUsername=findViewById(R.id.etUsername)
         etPassword=findViewById(R.id.etPassword)
+        
 
-        val intent = Intent(this, ShowActivity::class.java)
+btnNext.setOnClickListener {
 
-        val user_name = "Alpha"
-        intent.putExtra("USER_NAME", user_name)
 
-        startActivity(intent)
+
+    val intent = Intent(this, ShowActivity::class.java)
+
+    val user_name = "Alpha"
+    intent.putExtra("USER_NAME", user_name)
+
+    startActivity(intent)
+
+
+}
+
 
 
 
